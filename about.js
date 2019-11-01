6b92369c17a506f033e985879d204af74c0388d2
Chris Abrams <abramsc32@gmail.com>
2019-11-01 09:55:38 -0400

    done without media q finished

--- about.js
+++ about.js
@@ -1,10 +1,13 @@
 window.onload = function () {
 
     function changeImage() {   
-        let BackgroundImg=["../golf/aboutimages/pebble.jpg","../golf/aboutimages/pebble2.jpg","../golf/aboutimages/noname.jpg",
-            "../golf/aboutimages/pine.jpg","../golf/aboutimages/sleephollow.jpeg","../golf/aboutimages/augusta.jpg","../golf/aboutimages/wm16th.jpg","../golf/aboutimages/cypress.jpg"]
+        let BackgroundImg=["../golf/aboutimages/pebble.jpg","../golf/aboutimages/pebble2.jpg",
+            "../golf/aboutimages/augusta.jpg","../golf/aboutimages/9.jpg","../golf/aboutimages/3.jpg","../golf/aboutimages/4.jpg","../golf/aboutimages/5.jpg","../golf/aboutimages/6.jpg",
+        ]
         let i = Math.floor((Math.random() * 8));
         document.body.style.backgroundImage = 'url("' + BackgroundImg[i] + '")';
     }
-    window.setInterval(changeImage, 4000);
+    window.setInterval(changeImage, 5000);
 }
+
+
