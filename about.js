4decfea2d5243e518b743e84e1025855d9a1d0a2
Chris Abrams <abramsc32@gmail.com>
2019-11-03 23:34:52 -0500

    done

--- about.js
+++ about.js
@@ -1,8 +1,8 @@
 window.onload = function () {
 
     function changeImage() {   
-        let BackgroundImg=["../golf/aboutimages/pebble.jpg","../golf/aboutimages/pebble2.jpg",
-            "../golf/aboutimages/augusta.jpg","../golf/aboutimages/9.jpg","../golf/aboutimages/3.jpg","../golf/aboutimages/4.jpg","../golf/aboutimages/5.jpg","../golf/aboutimages/6.jpg",
+        let BackgroundImg=["./aboutimages/pebble.jpg","./aboutimages/pebble2.jpg",
+            "./aboutimages/augusta.jpg","./aboutimages/9.jpg","./aboutimages/3.jpg","./aboutimages/4.jpg","./aboutimages/5.jpg","./aboutimages/6.jpg",
         ]
         let i = Math.floor((Math.random() * 8));
         document.body.style.backgroundImage = 'url("' + BackgroundImg[i] + '")';
